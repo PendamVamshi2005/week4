79d48f703707277403d9b1ad9f087924a9effb69
koushik7613 <koushik7613@gmail.com>
2026-02-03 11:25:30 +0530

    fff

--- expense-tracker-main/src/Context/AppReducer.jsx
+++ expense-tracker-main/src/Context/AppReducer.jsx
@@ -1,16 +1,22 @@
-export default (state, action) => {
-    switch(action.type){
-        case 'DELETE_TRANSACTION': 
-        return {
-            ...state,
-            transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-        }
+const AppReducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(
+                    transaction => transaction.id !== action.payload
+                )
+            }
+
         case 'ADD_TRANSACTION':
-            return{
+            return {
                 ...state,
                 transactions: [action.payload, ...state.transactions]
             }
+
         default:
-            return state;
+            return state
     }
 }
+
+export default AppReducer
